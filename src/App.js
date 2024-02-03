f6893ab307c0fb21acc26fbf4a4dabca003a30dc
Ty <tyrellcarlton@gmail.com>
2024-02-03 07:09:53 -0800

    re-do of assignment -completed nav bar

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import React from 'react';
 import './App.css';
+import Nav from "./components/Nav"
 
 function App() {
   return (
- 
-    <div>
-      <h1>Meta Frontend Developer Capstone Starter Files</h1>
-    </div>
-  );
+ <>
+
+    <Nav/>
+     
+     </>
+  )
 }
 
 export default App;
---
+++ src/components/Nav.js
@@ -0,0 +1,41 @@
+import React, { useState } from "react";
+import logo from "../images/Logo .svg";
+
+const  Nav = ()=> {
+    const [menuOpen,setMenuOpen] = useState(false)
+    const toggleMenu = () =>{
+        setMenuOpen(!menuOpen);
+    }
+    return (
+    <nav className={`navbar ${menuOpen ? "open" : ""}`}>
+     <a href='/' className="logo"> <img src={logo} alt="logo"></img>
+        </a>
+        {/* mobile avatar*/}
+        <div className="menu-icon" onClick={toggleMenu}>
+        <div className="bar"></div>
+        <div className="bar"></div>
+        <div className="bar"></div>
+        </div>
+        {/* nav items*/}
+  <ul className={`nav-links ${menuOpen ? "visible" : ""}` }>
+    <li>
+        <a href="/"> Home</a>        
+    </li>
+    <li>
+        <a href="/"> About</a>        
+    </li>
+    <li>
+        <a href="/"> Services</a>        
+    </li>
+    <li>
+        <a href="/"> Reservations</a>        
+    </li>
+    <li>
+        <a href="/"> Order Online</a>        
+    </li>
+  </ul>
+  </nav>
+);
+}
+
+export default Nav
